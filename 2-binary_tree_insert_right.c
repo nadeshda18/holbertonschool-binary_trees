f23eb7adb8a2c05c1e76fe6b42b1d59a5e1be5d9
nadeshda18 <nadeshda02@hotmail.com>
2023-09-12 15:02:36 +0000

    insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,10 +8,12 @@
  *
  * Return: a pointer or NULL on failure or if parent is NULL
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
@@ -20,13 +22,20 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->right = parent->right;
 	new_node->left = NULL;
 
 	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
+	}
+	else
+	{
+		new_node->right = NULL;
+	}
 
 	parent->right = new_node;
 
 	return (new_node);
 }
+

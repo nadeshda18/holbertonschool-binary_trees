7f5b5b3f62f5389be906fc88f40045b00c1f6343
nadeshda18 <nadeshda02@hotmail.com>
2023-09-12 13:34:35 +0000

    preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - using preorder traversal
+ * @tree: pointer to the root node
+ * @func: pointer to a function
+ *
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}

90cabde455d110c948ab04c1f4a527381b9ab53e
nadeshda18 <nadeshda02@hotmail.com>
2023-09-15 14:03:42 +0000

    BETTY

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -20,7 +20,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right) &&
 			binary_tree_depth(tree->left) == binary_tree_depth(tree->right))
 	{
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
 	}
 
 	else
